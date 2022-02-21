e78dcd593dd63240b6c6b32a0e2d8e4a36de2911
adhikary97 <adhikary@purdue.edu>
2022-02-20 17:52:14 -0800

    first commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
 
-export default function Footer() {
-    return (
-      <div className="footer">
-        <section className="footer-links content-container">
-          <div className="text-center p-4">
-            <a className="text-reset fw-bold" href="https://github.com/AkhileshThite/dtube/">GitHub</a>
-          </div>
-        </section>
-      </div>
-    );
-}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <section className="footer-links content-container">
+        <div className="text-center p-4">
+          <a
+            className="text-reset fw-bold"
+            href="https://github.com/adhikary97"
+          >
+            GitHub
+          </a>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
